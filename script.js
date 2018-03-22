96a4e43deecc27bb994f50b92c37ab5778738cb1
KevinDNF <kevinnavarrofaubla@gmail.com>
2018-03-22 14:46:19 +0000

    Collapsing footer

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleSidebar(){
     document.getElementById("content").classList.toggle("active");
 }
+function toggleFooter(){
+    document.getElementById("mainFooter").classList.toggle("expanded");
+}
